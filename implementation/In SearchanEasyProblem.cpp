b51b7d1dc135256bbf1034713ac678744ab9cabd
Devanshu Sharma <abhisim0098@gmail.com>
2022-03-02 20:26:04 +0530

    1030A - In Search of an Easy Problem

---
+++ implementation/In SearchanEasyProblem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  while (n--)
+  {
+    int a;
+    cin >> a;
+    if (a == 1)
+    {
+      cout << "HARD";
+      return 0;
+    }
+  }
+  cout << "EASY";
+  return 0;
+}
